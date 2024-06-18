6c09b934c3e1ee078db12fd95aede520bab7636f
kauabilotti <kabilottialberton@gmail.com>
2024-06-18 09:30:01 -0300

    Criei uma funcao para fazer o botao de entrar do cadastro ir para a tela de inicio

---
+++ scripts/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('Entrar').addEventListener('click', function() {
+      window.location.href = 'file:///C:/Users/kaua_bilotti/Documents/SA_final/Trabalho-SA/estruturas/inicio.html'; // voce vai fornecer a url da pagina que voce quer e ele te redireciona para a pagina de sua escolha
+    });
+  });
+  
